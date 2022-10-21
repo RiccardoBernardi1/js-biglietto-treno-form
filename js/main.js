fd27276e94b59b873179eb168eda0e25296439b6
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-21 15:26:39 +0200

    Crea algoritmo calcolo prezzo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 "use strict"
+// 1. Chiedere all'utente nome e cognome
+const name=document.getElementById("name").value;
+console.log(name);
+// 2. Chiedere all'utente i km da percorerre
+const km=Number(document.getElementById("km").value);
+console.log(km);
+// 3. Far scegliere all'utente la fascia d'età del passeggero
+const age=document.getElementById("age").value;
+console.log(age);
+// 4. Calcolare il prezzo del biglietto
+let price= km * 0.21
+if (age==="under-age"){
+    price-=price*0.2;
+    console.log(price+(" minorenne"))
+}else if (age==="over-65"){
+    price-=price*0.4;
+    console.log(price+(" over 65"))
+}else{
+    console.log(price+" adulto")
+}
+// 5. Mostrare a schermo il biglietto
